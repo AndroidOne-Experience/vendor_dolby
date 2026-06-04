88fd21e7dbd97eee8bced0956aa844355ba3e541
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-06-04 22:48:45 +0530

    dolby: Import Dolby soundfx binaries from Sony
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- prebuilts/prebuilts-vendor.mk
+++ prebuilts/prebuilts-vendor.mk
@@ -7,19 +7,30 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/dolby/prebuilts/proprietary/vendor/etc/init/dms-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dms-service.rc \
+    vendor/dolby/prebuilts/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
     vendor/dolby/prebuilts/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc \
     vendor/dolby/prebuilts/proprietary/vendor/etc/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 PRODUCT_PACKAGES += \
+    libdapparamstorage-sony \
     libdapparamstorage \
+    libdlbpreg \
     libdmshal \
+    libdlbvol \
+    libswdap \
+    libswgamedap \
+    libswvqe \
     vendor.dolby.dms-V1-ndk \
+    vendor.dolby.hardware.dms@2.0-sony \
     vendor.dolby.hardware.dms@2.0 \
     vendor.dolby.hardware.dms@2.1 \
     libcodec2_soft_ac4dec \
     libcodec2_soft_ddpdec \
     libcodec2_store_dolby \
     libdeccfg \
+    libdlbdsservice-sony \
     libdlbdsservice \
+    vendor.dolby.hardware.dms@2.0-impl \
     vendor.dolby.dms.service \
+    vendor.dolby.hardware.dms@2.0-service \
     vendor.dolby.media.c2@1.0-service
